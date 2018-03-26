45a97c7770f6567c8fbfcce532db793e48424919
huxinmin <744019705@qq.com>
2018-03-26 12:11:54 +0800

    index add mainnav and recomand musics

---
+++ src/mixins/urls.js
@@ -0,0 +1,8 @@
+import wepy from 'wepy'
+
+export default class Urls extends wepy.mixin {
+  data = {
+    swiperUrl: "http://127.0.0.1:3000/banner",
+    recomandMusicsUrl:"http://127.0.0.1:3000/personalized"
+  }
+}
