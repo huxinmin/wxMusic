34bc678e6e76339f204235a5ebf87bc506056ccf
huxinmin <744019705@qq.com>
2018-03-26 16:56:42 +0800

    private fm

--- src/mixins/urls.js
+++ src/mixins/urls.js
@@ -3,6 +3,8 @@ import wepy from 'wepy'
 export default class Urls extends wepy.mixin {
   data = {
     swiperUrl: "http://127.0.0.1:3000/banner",
-    recomandMusicsUrl:"http://127.0.0.1:3000/personalized"
+    recomandMusicsUrl:"http://127.0.0.1:3000/personalized",
+    personalFmUrl:"http://musicapi.leanapp.cn/personal_fm",
+    musicUrl:"http://musicapi.leanapp.cn/music/url?id="
   }
 }
