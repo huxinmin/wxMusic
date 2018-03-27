87377ed2d925434fa0ce666102b8087703a728af
huxinmin <744019705@qq.com>
2018-03-27 13:31:16 +0800

    rank page

--- src/mixins/localData.js
+++ src/mixins/localData.js
@@ -2,14 +2,14 @@ import wepy from 'wepy'
 
 export default class LocalData extends wepy.mixin {
   data = {
-    rank:[{name:"云音乐新歌榜", url:""},{name:"云音乐热歌榜",url:""},{name:"网易原创歌曲榜",url:""},
-		  {name:"云音乐飙升榜",url:""},{name:"云音乐电音榜",url:""},{name:"UK排行榜周榜",url:""},
-		  {name:"美国Billboard周榜",url:""},{name:"KTV嗨榜",url:""},{name:"iTunes榜",url:""},{name:"Hit FM Top榜",url:""},
-		  {name:"日本Oricon周榜",url:""},{name:"韩国Melon排行榜周榜",url:""},{name:"韩国Mnet排行榜周榜",url:""},
-		  {name:"韩国Melon原声周榜",url:""},{name:"中国TOP排行榜(港台榜)",url:""},{name:"中国TOP排行榜(内地榜)",url:""},
-		  {name:"香港电台中文歌曲龙虎榜",url:""},{name:"华语金曲榜",url:""},{name:"中国嘻哈榜",url:""},
-		  {name:"法国 NRJ EuroHot 30周榜",url:""},{name:"台湾Hito排行榜",url:""},
-		  {name:"Beatport全球电子舞曲榜",url:""},{name:"云音乐ACG音乐榜",url:""},{name:"云音乐嘻哈榜",url:""}
+    rank:[{name:"云音乐新歌榜"},{name:"云音乐热歌榜"},{name:"网易原创歌曲榜"},
+		  {name:"云音乐飙升榜"},{name:"云音乐电音榜"},{name:"UK排行榜周榜"},
+		  {name:"美国Billboard周榜"},{name:"KTV嗨榜"},{name:"iTunes榜"},{name:"Hit FM Top榜"},
+		  {name:"日本Oricon周榜"},{name:"韩国Melon排行榜周榜"},{name:"韩国Mnet排行榜周榜"},
+		  {name:"韩国Melon原声周榜"},{name:"中国TOP排行榜(港台榜)"},{name:"中国TOP排行榜(内地榜)"},
+		  {name:"香港电台中文歌曲龙虎榜"},{name:"华语金曲榜"},{name:"中国嘻哈榜"},
+		  {name:"法国 NRJ EuroHot 30周榜"},{name:"台湾Hito排行榜"},
+		  {name:"Beatport全球电子舞曲榜"},{name:"云音乐ACG音乐榜"},{name:"云音乐嘻哈榜"}
 		]
   }
 }
