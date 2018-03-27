b70398ffccf84603401b4f4663daeed31c50c2da
huxinmin <744019705@qq.com>
2018-03-27 15:09:31 +0800

    rank song lists

--- src/mixins/urls.js
+++ src/mixins/urls.js
@@ -2,9 +2,6 @@ import wepy from 'wepy'
 
 export default class Urls extends wepy.mixin {
   data = {
-    swiperUrl: "http://musicapi.leanapp.cn/banner",
-    recomandMusicsUrl:"http://musicapi.leanapp.cn/personalized",
-    personalFmUrl:"http://musicapi.leanapp.cn/personal_fm",
-    musicUrl:"http://musicapi.leanapp.cn/music/url?id="
+    baseUrl: "http://127.0.0.1:3000"
   }
 }
